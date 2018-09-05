bb7d22bc27c805821ec488a205d0c1a768089bed
gtanno3 <gentanno@lawn-143-215-55-51.lawn.gatech.edu>
2018-09-04 21:50:39 -0400

    trying to merge conflict
    added name and gt id

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,7 +73,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("gburdell4");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("Gen Mark Tanno");
+                  Person5 p5 = new Person5("Gen Tanno");
                   toPrint += p5.toString("gtanno3");
 
         System.out.println(toPrint);
